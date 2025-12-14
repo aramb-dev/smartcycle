522d65e56b585cd382c211db1fbaac4c3337db96
Abdur Rahman Bilal <findarambilal@gmail.com>
2025-12-14 13:58:59 -0500

    feat(module2): add new battery and GPS components to smartcycle storage

--- module2/main.py
+++ module2/main.py
@@ -11,5 +11,17 @@ smartcycle_components = {
     "RST007": {"name": "Fusible Fixed Resistor 5% 10Ω", "quantity": 0},
     "RST008": {"name": "Linear Potentiometer 10% 10kΩ", "quantity": 56},
     "RST009": {"name": "Precision Linear Resistor 1% 470Ω", "quantity": 34},
-    
+    "BMS001": {"name": "TI bq76952 16-Cell Battery Monitor - High accuracy voltage/temp monitoring for Li-ion packs", "quantity": 10},
+    "BMS002": {"name": "Maxim MAX17320 ModelGauge m5 - Smart battery fuel gauge with protector", "quantity": 20},
+    "BMS003": {"name": "Analog Devices LTC6811 Multi-Cell Monitor - 12-cell battery stack monitor IC", "quantity": 15},
+    "BMS004": {"name": "Infineon TLE9012AQU - 12-channel battery cell controller with integrated balancing", "quantity": 5},
+    "BMS005": {"name": "NXP MC33771B 14-Cell Monitor - Automotive grade battery management controller", "quantity": 3},
+    "BMS006": {"name": "Renesas ISL94202 - Multi-cell Li-ion battery pack monitor with FET control", "quantity": 10},
+    "GPS001": {"name": "u-blox NEO-M9N - Multi-GNSS receiver with concurrent GPS/GLONASS/Galileo/BeiDou", "quantity": 42},
+    "GPS002": {"name": "Quectel L76-L - Ultra-compact GPS/GLONASS module with integrated antenna", "quantity": 67},
+    "GPS003": {"name": "MediaTek MT3333 - High sensitivity GPS chipset with 99 channels", "quantity": 28},
+    "GPS004": {"name": "STMicroelectronics Teseo-LIV3F - Tiny GNSS module with dead reckoning", "quantity": 15},
+    "GPS005": {"name": "u-blox ZED-F9P - Multi-band RTK GNSS receiver with cm-level accuracy", "quantity": 8},
+    "GPS006": {"name": "SkyTraq Venus638FLPx - Low power GPS receiver with 1Hz-10Hz update rate", "quantity": 53},
+    "GPS007": {"name": "Trimble BD970 - Dual-frequency RTK GNSS OEM board for precision positioning", "quantity": 3},
 }
